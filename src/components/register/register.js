e9d50fbb052063114558c0dc7a461f161338db3f
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-05 20:25:45 -0600

    set up skelleton for the register component

---
+++ src/components/register/register.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the register component</p>
+      </div>
+    );
+  }
+}
+
+export default Register;
