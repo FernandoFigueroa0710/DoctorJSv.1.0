c30a966807dd84400b62c73bf2d9c07b019297d6
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-08 00:55:45 -0600

    add login and register components to container
    Testing the rendering of components and designned the login and register

--- src/container/App.js
+++ src/container/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import Login from "../components/login/login";
-import Register from "../components/register/register";
+// import Login from "../components/login/Login";
+// import Register from "../components/register/Register";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p />
           Welcome to Doctor JS v1.0 Powered by react
+          {/* <Login />
+          <Register /> */}
         </header>
       </div>
     );
