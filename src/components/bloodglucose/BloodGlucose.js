39eeb9f570e7c9c16ace95f8457197f5615eaefd
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-13 19:39:21 -0600

    Blood Glucose component created

---
+++ src/components/bloodglucose/BloodGlucose.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class BloodGlucose extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is the Blood Glocose component</p>
+      </div>
+    );
+  }
+}
