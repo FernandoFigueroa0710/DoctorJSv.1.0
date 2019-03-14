3f4e87f78ab9ce3100e7d01e2144f5d379a18146
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-13 19:36:02 -0600

    create Blood Pressure component

---
+++ src/components/bloodpressure/BloodPressure.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class BloodPressure extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is the Blood Pressure component</p>
+      </div>
+    );
+  }
+}
+
+export default BloodPressure;
