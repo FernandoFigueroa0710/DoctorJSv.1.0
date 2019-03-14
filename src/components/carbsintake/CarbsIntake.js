21ea57bc432f3cf59390a5f99457066c7c02629d
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2019-03-13 19:45:57 -0600

    additional components set up
    user and carbs intale component

---
+++ src/components/carbsintake/CarbsIntake.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CarbsIntake extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is the carbs reading component</p>
+      </div>
+    );
+  }
+}
+
+export default CarbsIntake;
---
+++ src/components/profile/User.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is the User component</p>
+      </div>
+    );
+  }
+}
+
+export default User;
